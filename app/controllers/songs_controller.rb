91ef6da184c7f2922248566f47ec46d4174266fe
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-05 22:57:00 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,6 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-  #  binding.pry
     @song = Song.new(name: params["Name"])
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params[:genres]
@@ -37,9 +36,10 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
+    @song.genre_ids = params[:genres]
+
     @song.update(params [:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    @song.genre_ids = params[:genres]
     @song.save
 
     flash[:message] = "Successfully updated song."
