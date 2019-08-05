e6fe165313679f27522c063de7a7af5f86b42a55
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-05 22:37:11 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    binding.pry
+  #  binding.pry
     @song = Song.new(params["Name"])
     @song.artist = Artist.find_or_create_by(params["Artist Name"])
     @song.genre_ids = params[:genres]
